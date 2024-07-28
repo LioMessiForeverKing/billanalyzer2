687a289d6bd2c30c2ffb421da476875810a0278a
BriLeighk <kirchgessner@wisc.edu>
2024-07-28 06:28:32 -0400

    added google form

--- bill-frontend/src/App.js
+++ bill-frontend/src/App.js
@@ -14,7 +14,21 @@ function App() {
                     <Route path="/dashboard" element={<BillDashboard />} />
                 </Routes>
                 <footer className="App-footer">
-                    @ Bri Kirchgessner | Ayen Monasha | Tridhatri Vallamkondu
+                    <div className="footer-content">
+                        <div className="google-form-container">
+                            <iframe
+                                src="https://docs.google.com/forms/d/e/1FAIpQLSdjqyMGtNWsc6rPZH3Wb5mwTj1Wmh74amPwTONvpg9qJXFJZg/viewform?embedded=true"
+                                width="100%"
+                                height="418"
+                                frameBorder="0"
+                                marginHeight="0"
+                                marginWidth="0"
+                            >
+                                Loading…
+                            </iframe>
+                        </div>
+                        <p className="trademark">@ Bri Kirchgessner | Ayen Monasha | Tridhatri Vallamkondu</p>
+                    </div>
                 </footer>
             </div>
         </Router>
